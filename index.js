45721aac56381c2667c0af735ae3965000877dd5
eliastooloee <elitooloee@gmail.com>
2020-01-29 17:21:35 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    editDOM()
+  });
+
+  function editDOM() {
+      document.getElementById("text")
+      .innerHTML = "This is really cool!";
+  }
